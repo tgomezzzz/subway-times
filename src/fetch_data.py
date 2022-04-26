51258be3a26537a4f3847aac51b6fe179633d93a
Tommy Gomez <tjk2132@columbia.edu>
2022-04-26 19:48:06 -0400

    get and print data for trains arriving at 116

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,5 +1,6 @@
 import requests
 import gtfs_realtime_pb2 as gtfs
+import datetime as dt
 
 # 1, 2, 3, 4, 5, 6, 7 trains
 url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs'
@@ -29,16 +30,29 @@ def getStations():
     except:
         print("Unable to read stations")
         return {}
-            
-
 
 def main():
     resp = requests.get(url, headers={'x-api-key': getKey()})
     stations = getStations()
 
-    proto = gtfs.FeedMessage()
-    proto.ParseFromString(resp.content)
-    print(stations)
+    train_info = gtfs.FeedMessage()
+    train_info.ParseFromString(resp.content)
+    
+    for entity in train_info.entity:
+        if entity.HasField('trip_update'):
+            trip_update = entity.trip_update
+            if trip_update.HasField('trip'):
+                trip = trip_update.trip
+                if trip.HasField('route_id') and trip.route_id == "1":
+                    for update in trip_update.stop_time_update:
+                        if update.HasField('stop_id') and update.stop_id == "117N":
+                            if update.HasField('arrival') and update.arrival.HasField('time'):
+                                time = dt.datetime.fromtimestamp(update.arrival.time)
+                                print("Found Northbound 1 train arriving at", time)
+                        elif update.HasField('stop_id') and update.stop_id == "117S":
+                            if update.HasField('arrival') and update.arrival.HasField('time'):
+                                time = dt.datetime.fromtimestamp(update.arrival.time)
+                                print("Found Southbound 1 train arriving at", time)
 
 if __name__ == '__main__':
     main()
