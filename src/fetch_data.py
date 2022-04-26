bf951a392c261879c3bc7d88ab6f79654484cce4
Tommy Gomez <tjk2132@columbia.edu>
2022-04-26 18:54:29 -0400

    get station data

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,25 +1,44 @@
 import requests
 import gtfs_realtime_pb2 as gtfs
 
-# 1, 2, 3, 4, 5, 6, 7
+# 1, 2, 3, 4, 5, 6, 7 trains
 url = 'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/nyct%2Fgtfs'
-KEYPATH = '../key.txt'
+KEY_PATH = '../key.txt'
+STATIONS_PATH = '../data/stops.txt'
 
 def getKey():
     try:
-        f = open(KEYPATH, 'r')
+        f = open(KEY_PATH, 'r')
         key = f.read()
         f.close()
         return key
     except:
         raise Exception("Unable to find MTA API key")
 
+def getStations():
+    try:
+        f = open(STATIONS_PATH, 'r')
+        f.readline()
+        stations = {}
+        for line in f:
+            data = line.split(',')
+            if len(data) > 0 and data[0] != '' and data[2] != '':
+                stations[data[0]] = data[2]
+        f.close()
+        return stations
+    except:
+        print("Unable to read stations")
+        return {}
+            
+
+
 def main():
     resp = requests.get(url, headers={'x-api-key': getKey()})
+    stations = getStations()
 
     proto = gtfs.FeedMessage()
     proto.ParseFromString(resp.content)
-    print(proto)
+    print(stations)
 
 if __name__ == '__main__':
     main()
