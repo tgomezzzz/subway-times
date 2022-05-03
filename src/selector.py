b968f0753c920af9fc9c40e251ab832026271fcf
Tommy Gomez <tjk2132@columbia.edu>
2022-05-02 21:50:35 -0400

    change selection to automatically adjust display times

--- src/selector.py
+++ src/selector.py
@@ -4,6 +4,7 @@ class Selector:
     def __init__(self, x, y, w, h, options, win):
         self.rect = Rectangle(Point(x, y), Point(x + w, y + h))
         self.rect.setFill(color_rgb(79, 79, 79))
+        self.rect.setWidth(4)
         self.rect.setOutline("")
         self.rect.draw(win)
         self.win = win
@@ -12,7 +13,7 @@ class Selector:
         self.selected = [-1, None]
         if len(self.options) > 0:
             self.selected = [0, self.options[0]]
-        self.is_selected = False
+        self.in_use = False
 
     def addOption(self, option):
         self.options.append(option)
@@ -38,13 +39,45 @@ class Selector:
             return
         self.setSelected(self.selected[0] + dir)
 
+    def choose(self):
+        self.in_use = True
+        self.rect.setOutline('white')
+
+    def unchoose(self):
+        self.in_use = False
+        self.rect.setOutline('')
+
     def getCenter(self):
         return self.rect.getCenter()
 
 class TrainSelector(Selector):
+    def selectNext(self, dir):
+        if len(self.options) == 0:
+            return
+        return self.setSelected(self.selected[0] + dir)
+
     def setSelected(self, i):
         super().setSelected(i)
+        names, data = self.selected[1].fetchData()
+        return names, data
 
 class StationSelector(Selector):
+    def selectNext(self, dir):
+        if len(self.options) == 0:
+            return
+        return self.setSelected(self.selected[0] + dir)
+
     def setSelected(self, i):
         super().setSelected(i)
+        return self.selected[1].getText()
+
+    def setOptions(self, options):
+        self.options = []
+        for option in options:
+            textBox = Text(self.getCenter(), option)
+            textBox.setFill('white')
+            textBox.setSize(36)
+            self.options.append(textBox)
+        if len(self.options) > 0:
+            self.setSelected(0)
+        return self.selected[1].getText()
